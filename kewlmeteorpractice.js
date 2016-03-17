bd6cc48aabb98007d85c549650fb42d23455f8a1
Wyatt Elliott <wyattelliott27@gmail.com>
2016-03-17 07:25:08 +0000

    color checkboxes working well

--- kewlmeteorpractice.js
+++ kewlmeteorpractice.js
@@ -1,7 +1,20 @@
 Cars = new Mongo.Collection ('cars')
+var colorArray = [];
 
 
 if (Meteor.isClient) {
+
+
+
+
+
+
+
+
+
+
+
+
 
 
   Template.body.helpers({
@@ -10,27 +23,76 @@ if (Meteor.isClient) {
       if (Session.get('showAll')){
         return Cars.find()
       }
-      if (Session.get('showColor')){
-        return Cars.find({ carColor : "Silver"})
+      
+      if (Session.get('showBlack')){ //if black is checked
+       colorArray.push('Black')
+        return Cars.find({ carColor : {$in: colorArray} })
+      }else{ // if black is not checked
+        for (var i=colorArray.length-1; i>=0; i--) {
+           if (colorArray[i] === 'Black') {
+        colorArray.splice(i, 1);
+          }
+        }
       }
-      if (Session.get('showBlack')){
-        return Cars.find({ carColor : "Black"})
+      
+      if (Session.get('showRed')){ //if black is checked
+       colorArray.push('Red')
+        return Cars.find({ carColor : {$in: colorArray} })
+      }else{ // if black is not checked
+        for (var i=colorArray.length-1; i>=0; i--) {
+           if (colorArray[i] === 'Red') {
+        colorArray.splice(i, 1);
+          }
+        }
       }
-      if (Session.get('showRed')){
-        return Cars.find({ carColor : "Red"})
+      
+      
+      if (Session.get('showOrange')){ //if black is checked
+       colorArray.push('Orange')
+        return Cars.find({ carColor : {$in: colorArray} })
+      }else{ // if black is not checked
+        for (var i=colorArray.length-1; i>=0; i--) {
+           if (colorArray[i] === 'Orange') {
+        colorArray.splice(i, 1);
+        }
       }
-      if (Session.get('showOrange')){             
-        return Cars.find({ carColor : "Orange"})           
-      } 
-      if (Session.get('showBlue')){
-        return Cars.find({ carColor : "Blue"})
+    }
+      
+      
+      if (Session.get('showBlue')){ //if black is checked
+       colorArray.push('Blue')
+        return Cars.find({ carColor : {$in: colorArray} })
+      }else{ // if black is not checked
+        for (var i=colorArray.length-1; i>=0; i--) {
+           if (colorArray[i] === 'Blue') {
+        colorArray.splice(i, 1);
+        }
       }
-      if (Session.get('showSubaru')){
-        return Cars.find({})
-      }
+     }     
+      
  //     return Cars.find();
     }
   });
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
   
   
   
